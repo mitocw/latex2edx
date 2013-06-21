42933a0bdea2126d3c013b77b9a6467800fecbf7
celieber <celieber@garela>
2013-06-21 12:59:48 -0400

    working equation references with pop up that renders

--- latex2edx.py
+++ latex2edx.py
@@ -624,7 +624,7 @@ def handle_equation_labels_and_refs(tree):
     for chapter in tree.findall('.//chapter'):
         modulenum = modulenum + 1
         eqnnum = 1  # counter for equation numbering
-        for table in tree.findall('.//table'):
+        for table in chapter.findall('.//table'):
             if table.get('class') == 'equation':  # handle equation
                 for tr in table.findall('.//tr'):
                     for td in tr.findall('.//td'):
@@ -649,10 +649,19 @@ def handle_equation_labels_and_refs(tree):
                             if a.text == eqnlabel:
                                 # change this ref element
                                 a.text = "%d.%d" % (modulenum,eqnnum)
-                                a.set('href',"#")
-                                tablestr = etree.tostring(table,encoding="US-ASCII",method="html")
-                                #htmlstr = "<html><head></head><body>%s<script type=\"text/javascript\" src=\"https://edx-static.s3.amazonaws.com/mathjax-MathJax-07669ac/MathJax.js?config=TeX-MML-AM_HTMLorMML-full\"></script></body></html>" % tablestr
-                                htmlstr = "<html><head></head><body><h1>Hello</h1></body></html>"
+                                #a.set('href',"#")
+                                tablestr = etree.tostring(table,encoding="utf-8",method="html")
+                                #htmlstr = "\'<html><head></head><body>%s</body></html>\'" % tablestr
+                                #print tablestr
+                                tablestr_find = re.search(r'\[mathjax\](.*?)\[/mathjax\]',tablestr,re.S)
+                                tablestr = "<p>$$" + re.escape(tablestr_find.group(1).encode("US-ASCII")) + "$$</p>"
+                                mathjax = "<script type=\"text/javascript\" src=\"https://c328740.ssl.cf1.rackcdn.com/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML\"> </script>"
+                                #mathjax = "<script type=\"text/javascript\" src=\"http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML\"> </script>"
+                                #mathjax = "(function () { var script = document.createElement(\"script\"); script.type = \"text/javascript\"; script.src  = \"http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML-full\"; var config = 'MathJax.Hub.Startup.onload();'; if (window.opera) {script.innerHTML = config} else {script.text = config} document.getElementsByTagName(\"head\")[0].appendChild(script); })();"
+                                htmlstr = "\'<html><head>%s</head><body>%s</body></html>\'" % (mathjax,tablestr)
+                                print htmlstr  
+                                #raw_input("Press ENTER")
+                                
                                 onClick = "return newWindow(%s)" % htmlstr
                                 a.set('onClick',onClick)
 
