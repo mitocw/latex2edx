f6484f03a9a6f7d80a8e2bb692f8f3c536b5d922
celieber <celieber@garela>
2013-06-26 15:48:50 -0400

    js for latex2edx new windows

--- latex2edx.js
+++ latex2edx.js
@@ -9,30 +9,3 @@ function newWindow(html,eqnstr) {
     }
 }
 
-// Add event listeners to allow edX onload functions to run in addition to our own (specified below)
-if (window.addEventListener) { // Mozilla, Netscape, Firefox
-    window.addEventListener('load', WindowLoad, false);
-} else if (window.attachEvent) { // IE
-    window.attachEvent('onload', WindowLoad);
-}
-
-// Our own onload function to listen for DOMSubtree modifications to call another javascript function that modifies the feedback "Correct" or "Incorrect" for the justification response section
-function WindowLoad(event) {
-    process_justifications();
-    $("#seq_content").bind("DOMSubtreeModified", function() {
-        process_justifications();
-    });
-}
-
-function process_justifications() {
-    var debugs = document.getElementsByClassName("debug");
-    for (var i=0; i<debugs.length; i++) {
-        if (debugs[i].innerHTML == "incorrect") {
-             debugs[i].innerHTML = "provide justification";
-        }
-        if (debugs[i].innerHTML == "correct") {
-            debugs[i].innerHTML = "justification submitted";
-        }
-    }
-}
-
