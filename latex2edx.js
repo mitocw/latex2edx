59275ac3683c6a4a5f735f741f6128393cae59b4
celieber <celieber@garela>
2013-07-17 11:39:12 -0400

    updated supporting js

--- latex2edx.js
+++ latex2edx.js
@@ -1,5 +1,5 @@
 // Function for making new windows with specified html content
-function newWindow(html,eqnstr) {
+window.newWindow = function(html,eqnstr) {
     var windowRef = window.open("",eqnstr,"height=100,width=400");
     if (windowRef) {
         var d = windowRef.document;
