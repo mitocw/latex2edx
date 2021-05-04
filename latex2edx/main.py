7db0097dde1003f9c37583d09a54bf5d22c27b4c
JenFrench <jfrench@mit.edu>
2021-05-04 14:38:26 -0400

    fix location of caret with respect to text of h4 element

--- latex2edx/main.py
+++ latex2edx/main.py
@@ -1416,7 +1416,7 @@ class latex2edx(object):
             sub2 = etree.SubElement(sub1, 'span',
                              {'class': 'arrow down'})
             sub2.text = ' '
-            sub1.tail = desc
+            sub2.tail = desc
             newsh.append(showhide)
             showhide.tag = 'div'  # change edxshowhide tag
             if 'description' in showhide.attrib:
